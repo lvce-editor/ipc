3bce7549e4f8251025bcbc4f87b9a83d0a51acb8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-02 14:44:37 +0200

    feature: bundle package (#13)

--- src/parts/WebSocketServer/WebSocketServer.js
+++ src/parts/WebSocketServer/WebSocketServer.js
@@ -1,1 +1,5 @@
-export * from '@lvce-editor/web-socket-server'
+export const handleUpgrade = async (...args) => {
+  const module = await import('@lvce-editor/web-socket-server')
+  // @ts-ignore
+  return module.handleUpgrade(...args)
+}
