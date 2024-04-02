0ac9a06b993f659ef2a4920c8837fdde1e023fcd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-02 16:17:17 +0200

    feature: add typescript types (#24)

---
+++ src/index.d.ts
@@ -0,0 +1,18 @@
+interface IpcChild {
+  readonly listen: any
+  readonly wrap: any
+}
+
+export const IpcChildWithElectronMessagePort: IpcChild
+export const IpcChildWithElectronUtilityProcess: IpcChild
+export const IpcChildWithNodeForkedProcess: IpcChild
+export const IpcChildWithWebSocket: IpcChild
+
+interface IpcParent {
+  readonly create: any
+  readonly wrap: any
+}
+
+export const IpcParentWithElectronUtilityProcess: IpcParent
+export const IpcParentWithNodeForkedProcess: IpcParent
+export const IpcParentWithNodeWorker: IpcParent
