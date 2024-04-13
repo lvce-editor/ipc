91f1007e9a4d3532803fe6e54bd8d303eacfdcea
Le Vivilet <levivlet@pm.me>
2024-04-13 10:50:28 +0200

    fix: missing message event listener for node ipc

--- src/parts/GetFirstNodeWorkerEvent/GetFirstNodeWorkerEvent.ts
+++ src/parts/GetFirstNodeWorkerEvent/GetFirstNodeWorkerEvent.ts
@@ -1,9 +1,10 @@
 import * as FirstNodeWorkerEventType from '../FirstNodeWorkerEventType/FirstNodeWorkerEventType.ts'
 import * as GetFirstEvent from '../GetFirstEvent/GetFirstEvent.ts'
+import type { Worker } from 'node:worker_threads'
 
-// @ts-ignore
-export const getFirstNodeWorkerEvent = (worker) => {
+export const getFirstNodeWorkerEvent = (worker: Worker) => {
   return GetFirstEvent.getFirstEvent(worker, {
+    message: FirstNodeWorkerEventType.Message,
     exit: FirstNodeWorkerEventType.Exit,
     error: FirstNodeWorkerEventType.Error,
   })
