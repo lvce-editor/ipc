adc7a1738501d5131d012fa7de00a0a5172ca2f1
Le Vivilet <levivlet@pm.me>
2024-04-21 12:59:23 +0200

    feature: use event target api for node ipc

--- src/parts/IpcChildWithElectronUtilityProcess/IpcChildWithElectronUtilityProcess.ts
+++ src/parts/IpcChildWithElectronUtilityProcess/IpcChildWithElectronUtilityProcess.ts
@@ -1,4 +1,5 @@
 import * as GetUtilityProcessPortData from '../GetUtilityProcessPortData/GetUtilityProcessPortData.ts'
+import { Ipc } from '../Ipc/Ipc.ts'
 import * as ReadyMessage from '../ReadyMessage/ReadyMessage.ts'
 
 export const listen = () => {
@@ -10,48 +11,10 @@ export const listen = () => {
   return parentPort
 }
 
-// @ts-ignore
-export const signal = (parentPort) => {
+export const signal = (parentPort: any) => {
   parentPort.postMessage(ReadyMessage.readyMessage)
 }
 
-// @ts-ignore
-export const wrap = (parentPort) => {
-  return {
-    parentPort,
-    // @ts-ignore
-    on(event, listener) {
-      if (event === 'message') {
-        // @ts-ignore
-        const wrappedListener = (event) => {
-          const actualData = GetUtilityProcessPortData.getUtilityProcessPortData(event)
-          const syntheticEvent = {
-            data: actualData,
-            target: this,
-          }
-          listener(syntheticEvent)
-        }
-        this.parentPort.on(event, wrappedListener)
-      } else if (event === 'close') {
-        this.parentPort.on('close', listener)
-      } else {
-        throw new Error('unsupported event type')
-      }
-    },
-    // @ts-ignore
-    off(event, listener) {
-      this.parentPort.off(event, listener)
-    },
-    // @ts-ignore
-    send(message) {
-      this.parentPort.postMessage(message)
-    },
-    // @ts-ignore
-    sendAndTransfer(message, transfer) {
-      this.parentPort.postMessage(message, transfer)
-    },
-    dispose() {
-      this.parentPort.close()
-    },
-  }
+export const wrap = (parentPort: any) => {
+  return new Ipc(parentPort, GetUtilityProcessPortData.getUtilityProcessPortData)
 }
--- test/Ipc.test.ts
+++ test/Ipc.test.ts
@@ -2,6 +2,7 @@ import { Ipc } from '../src/parts/Ipc/Ipc.ts'
 import { test, expect } from '@jest/globals'
 import * as Promises from '../src/parts/Promises/Promises.ts'
 import { jest } from '@jest/globals'
+import { EventEmitter } from 'node:events'
 
 test('name', () => {
   expect(Ipc.name).toBe('Ipc')
@@ -23,6 +24,20 @@ test('addEventListener onmessage', async () => {
   port2.close()
 })
 
+test('addEventListener EventEmitter', async () => {
+  const emitter = new EventEmitter()
+  const getData = (data: any) => {
+    return data
+  }
+  const ipc = new Ipc(emitter, getData)
+  const { resolve, promise } = Promises.withResolvers<MessageEvent>()
+  ipc.addEventListener('message', resolve)
+  emitter.emit('message', 1)
+  const message = await promise
+  expect(message.data).toBe(1)
+  emitter.removeAllListeners()
+})
+
 test('set onmessage', async () => {
   const { port1, port2 } = new MessageChannel()
   const ipc = new Ipc(port1, getData)
@@ -35,9 +50,26 @@ test('set onmessage', async () => {
   port2.close()
 })
 
-test('send message', async () => {
+test('send message - postMessage', async () => {
   const { port1, port2 } = new MessageChannel()
   const ipc = new Ipc(port1, getData)
+  const { resolve, promise } = Promises.withResolvers<MessageEvent>()
+  port2.addEventListener('message', resolve)
+  ipc.send(1)
+  const message = await promise
+  expect(message.data).toBe(1)
+  port1.close()
+  port2.close()
+})
+
+test('send message - send', async () => {
+  const { port1, port2 } = new MessageChannel()
+  const socket = {
+    send(message: any) {
+      port1.postMessage(message)
+    },
+  }
+  const ipc = new Ipc(socket, getData)
   const { resolve, promise } = Promises.withResolvers<MessageEvent>()
   port2.addEventListener('message', resolve)
   ipc.send(1)
@@ -70,11 +102,20 @@ test('sendAndTransfer message - not supported', () => {
   expect(() => ipc.sendAndTransfer(1, [])).toThrow(new Error('sendAndTransfer not supported'))
 })
 
-test('dispose', () => {
+test('dispose - close', () => {
   const port = {
     close: jest.fn(),
   }
   const ipc = new Ipc(port, getData)
   ipc.dispose()
   expect(port.close).toHaveBeenCalledTimes(1)
+})
+
+test('dispose - kill', () => {
+  const process = {
+    kill: jest.fn(),
+  }
+  const ipc = new Ipc(process, getData)
+  ipc.dispose()
+  expect(process.kill).toHaveBeenCalledTimes(1)
 })
