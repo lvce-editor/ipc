6ce398dd11fca262f15b9d25711386cd77621c9b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-21 12:30:45 +0200

    feature: add ipc class to align ipc implementations and avoid duplica… (#44)

---
+++ test/Ipc.test.ts
@@ -0,0 +1,80 @@
+import { Ipc } from '../src/parts/Ipc/Ipc.ts'
+import { test, expect } from '@jest/globals'
+import * as Promises from '../src/parts/Promises/Promises.ts'
+import { jest } from '@jest/globals'
+
+test('name', () => {
+  expect(Ipc.name).toBe('Ipc')
+})
+
+const getData = (event: any) => {
+  return event.data
+}
+
+test('addEventListener onmessage', async () => {
+  const { port1, port2 } = new MessageChannel()
+  const ipc = new Ipc(port1, getData)
+  const { resolve, promise } = Promises.withResolvers<MessageEvent>()
+  ipc.addEventListener('message', resolve)
+  port2.postMessage(1)
+  const message = await promise
+  expect(message.data).toBe(1)
+  port1.close()
+  port2.close()
+})
+
+test('set onmessage', async () => {
+  const { port1, port2 } = new MessageChannel()
+  const ipc = new Ipc(port1, getData)
+  const { resolve, promise } = Promises.withResolvers<MessageEvent>()
+  ipc.onmessage = resolve
+  port2.postMessage(1)
+  const message = await promise
+  expect(message.data).toBe(1)
+  port1.close()
+  port2.close()
+})
+
+test('send message', async () => {
+  const { port1, port2 } = new MessageChannel()
+  const ipc = new Ipc(port1, getData)
+  const { resolve, promise } = Promises.withResolvers<MessageEvent>()
+  port2.addEventListener('message', resolve)
+  ipc.send(1)
+  const message = await promise
+  expect(message.data).toBe(1)
+  port1.close()
+  port2.close()
+})
+
+test('send message - not supported', () => {
+  const ipc = new Ipc({}, getData)
+  expect(() => ipc.send(1)).toThrow(new Error('send not supported'))
+})
+
+test('sendAndTransfer message', async () => {
+  const { port1, port2 } = new MessageChannel()
+  const ipc = new Ipc(port1, getData)
+  const { resolve, promise } = Promises.withResolvers<MessageEvent>()
+  port2.addEventListener('message', resolve)
+  const array = new Uint8Array([1])
+  ipc.sendAndTransfer(array, [array.buffer])
+  const message = await promise
+  expect(message.data).toEqual(new Uint8Array([1]))
+  port1.close()
+  port2.close()
+})
+
+test('sendAndTransfer message - not supported', () => {
+  const ipc = new Ipc({}, getData)
+  expect(() => ipc.sendAndTransfer(1, [])).toThrow(new Error('sendAndTransfer not supported'))
+})
+
+test('dispose', () => {
+  const port = {
+    close: jest.fn(),
+  }
+  const ipc = new Ipc(port, getData)
+  ipc.dispose()
+  expect(port.close).toHaveBeenCalledTimes(1)
+})
