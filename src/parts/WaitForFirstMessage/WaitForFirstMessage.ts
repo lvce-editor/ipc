28dc4bdf849c973ec54dba22a4b72167708f7f7a
Le Vivilet <levivlet@pm.me>
2024-04-28 10:15:56 +0200

    fix: waiting for first message

--- src/parts/WaitForFirstMessage/WaitForFirstMessage.ts
+++ src/parts/WaitForFirstMessage/WaitForFirstMessage.ts
@@ -1,16 +1,9 @@
 import * as Promises from '../Promises/Promises.ts'
 
-export const waitForFirstMessage = async (port: any) => {
-  const { resolve, promise } = Promises.withResolvers()
-  const cleanup = (value: any) => {
-    port.onmessage = null
-    resolve(value)
-  }
-  const handleMessage = (event: any) => {
-    cleanup(event)
-  }
-  port.onmessage = handleMessage
+export const waitForFirstMessage = async (port: EventTarget) => {
+  const { resolve, promise } = Promises.withResolvers<Event>()
+  port.addEventListener('message', resolve, { once: true })
   const event = await promise
-  // @ts-expect-error
+  // @ts-ignore
   return event.data
 }
