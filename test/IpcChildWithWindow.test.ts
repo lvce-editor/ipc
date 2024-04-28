a653f834ea4b778b43f5d90bac3230e1be9765fb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-28 08:34:56 +0200

    feature: ignore message events with ports in window ipc to avoid post… (#56)

---
+++ test/IpcChildWithWindow.test.ts
@@ -0,0 +1,83 @@
+import { expect, test } from '@jest/globals'
+import * as IpcChildWithWindow from '../src/parts/IpcChildWithWindow/IpcChildWithWindow.ts'
+
+test('onMessage - ignore events with messagePorts', async () => {
+  const mockWindow = new EventTarget()
+  const ipc = IpcChildWithWindow.wrap(mockWindow)
+
+  const messages: any[] = []
+  const handleMessage = (event: MessageEvent) => {
+    messages.push(event.data)
+  }
+  ipc.addEventListener('message', handleMessage)
+  const { port1 } = new MessageChannel()
+  mockWindow.dispatchEvent(
+    new MessageEvent('message', {
+      ports: [port1],
+    }),
+  )
+  expect(messages).toHaveLength(0)
+})
+
+test('onMessage - forward message events', async () => {
+  const mockWindow = new EventTarget()
+  const ipc = IpcChildWithWindow.wrap(mockWindow)
+
+  const messages: any[] = []
+  const handleMessage = (event: MessageEvent) => {
+    messages.push(event.data)
+  }
+  ipc.addEventListener('message', handleMessage)
+  mockWindow.dispatchEvent(
+    new MessageEvent('message', {
+      data: {
+        jsonrpc: '2.0',
+        id: 1,
+        result: 1,
+      },
+    }),
+  )
+  expect(messages).toEqual([
+    {
+      jsonrpc: '2.0',
+      id: 1,
+      result: 1,
+    },
+  ])
+})
+
+test('onMessage - ignore events after first message', async () => {
+  const mockWindow = new EventTarget()
+  const ipc = IpcChildWithWindow.wrap(mockWindow)
+
+  const messages: any[] = []
+  const handleMessage = (event: MessageEvent) => {
+    messages.push(event.data)
+  }
+  ipc.addEventListener('message', handleMessage)
+  mockWindow.dispatchEvent(
+    new MessageEvent('message', {
+      data: {
+        jsonrpc: '2.0',
+        id: 1,
+        result: 1,
+      },
+    }),
+  )
+  mockWindow.dispatchEvent(
+    new MessageEvent('message', {
+      data: {
+        jsonrpc: '2.0',
+        id: 2,
+        result: 2,
+      },
+    }),
+  )
+  expect(messages).toEqual([
+    {
+      jsonrpc: '2.0',
+      id: 1,
+      result: 1,
+    },
+  ])
+})
