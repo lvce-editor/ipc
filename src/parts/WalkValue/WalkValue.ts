487391a5e06bc922f455098e3e235630e208bae6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-24 19:29:12 +0200

    fix: undefined parameters when replacing electron transferrables (#81)

--- src/parts/WalkValue/WalkValue.ts
+++ src/parts/WalkValue/WalkValue.ts
@@ -1,4 +1,5 @@
 import * as IsTransferrable from '../IsTransferrable/IsTransferrable.ts'
+import * as UntransferrableValue from '../UntransferrableValue/UntransferrableValue.ts'
 
 export const walkValue = (value: unknown, transferrables: unknown[]) => {
   if (!value) {
@@ -6,13 +7,15 @@ export const walkValue = (value: unknown, transferrables: unknown[]) => {
   }
   if (IsTransferrable.isTransferrable(value)) {
     transferrables.push(value)
-    return undefined
+    return UntransferrableValue.UntransferrableValue
   }
   if (Array.isArray(value)) {
     const newItems = []
     for (const item of value) {
       const newItem: any = walkValue(item, transferrables)
-      newItems.push(newItem)
+      if (newItem !== UntransferrableValue.UntransferrableValue) {
+        newItems.push(newItem)
+      }
     }
     return newItems
   }
@@ -20,7 +23,9 @@ export const walkValue = (value: unknown, transferrables: unknown[]) => {
     const newObject = Object.create(null)
     for (const [key, property] of Object.entries(value)) {
       const newValue = walkValue(property, transferrables)
-      newObject[key] = newValue
+      if (newValue !== UntransferrableValue.UntransferrableValue) {
+        newObject[key] = newValue
+      }
     }
     return newObject
   }
