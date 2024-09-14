a0ca46afa2a7c976c877f406338a37a8cb08825c
Le Vivilet <levivlet@pm.me>
2024-09-14 21:15:59 +0200

    add test

--- test/IpcChildWithModuleWorkerAndMessagePort.test.ts
+++ test/IpcChildWithModuleWorkerAndMessagePort.test.ts
@@ -1,64 +1,63 @@
-import { expect, test } from '@jest/globals'
-import * as IpcChildWithMessagePort from '../src/parts/IpcChildWithMessagePort/IpcChildWithMessagePort.ts'
+import { expect, jest, test } from '@jest/globals'
+
+jest.unstable_mockModule('../src/parts/IpcChildWithModuleWorker/IpcChildWithModuleWorker.ts', () => {
+  const target = new EventTarget()
+  return {
+    target,
+    listen() {
+      return target
+    },
+    signal: jest.fn(),
+    wrap(port: any) {
+      return port
+    },
+  }
+})
+
+const IpcChildWithModuleWorkerAndMessagePort = await import(
+  '../src/parts/IpcChildWithModuleWorkerAndMessagePort/IpcChildWithModuleWorkerAndMessagePort.ts'
+)
+const IpcChildWithModuleWorker = await import('../src/parts/IpcChildWithModuleWorker/IpcChildWithModuleWorker.ts')
+
+test('listen - unexpected first message', async () => {
+  jest.spyOn(IpcChildWithModuleWorker, 'signal').mockImplementation(() => {
+    setTimeout(() => {
+      // @ts-ignore
+      IpcChildWithModuleWorker.target.dispatchEvent(
+        new MessageEvent('message', {
+          data: {},
+        }),
+      )
+    }, 0)
+  })
+  await expect(IpcChildWithModuleWorkerAndMessagePort.listen()).rejects.toThrow(new Error(`unexpected first message`))
+})
 
 test('listen - send back response', async () => {
-  const { port1, port2 } = new MessageChannel()
-  expect(IpcChildWithMessagePort.listen({ port: port1 })).toBe(port1)
-  port1.close()
-  port2.close()
-})
-
-test('signal', async () => {
-  const { port1, port2 } = new MessageChannel()
-  const promise = new Promise((resolve) => {
-    port2.onmessage = resolve
+  const port = {}
+  jest.spyOn(IpcChildWithModuleWorker, 'signal').mockImplementation(() => {
+    setTimeout(() => {
+      // @ts-ignore
+      IpcChildWithModuleWorker.target.dispatchEvent(
+        new MessageEvent('message', {
+          data: {
+            jsonrpc: '2.0',
+            method: 'initialize',
+            id: 1,
+            params: ['message-port', port],
+          },
+        }),
+      )
+    }, 0)
   })
-  IpcChildWithMessagePort.signal(port1)
-  const event = await promise
+  const send = jest.fn()
+  const dispose = jest.fn()
   // @ts-ignore
-  expect(event.data).toBe('ready')
-  port1.close()
-  port2.close()
-})
-
-test('wrap - send', async () => {
-  const { port1, port2 } = new MessageChannel()
-  const ipc = IpcChildWithMessagePort.wrap(port1)
-  const promise = new Promise((resolve) => {
-    port2.onmessage = resolve
+  jest.spyOn(IpcChildWithModuleWorker, 'wrap').mockImplementation(() => {
+    return {
+      send,
+      dispose,
+    }
   })
-  ipc.send('test')
-  const event = await promise
-  // @ts-ignore
-  expect(event.data).toBe('test')
-  port1.close()
-  port2.close()
-})
-
-test('wrap - sendAndTransfer', async () => {
-  const { port1, port2 } = new MessageChannel()
-  const ipc = IpcChildWithMessagePort.wrap(port1)
-  const promise = new Promise((resolve) => {
-    port2.onmessage = resolve
-  })
-  ipc.sendAndTransfer('test')
-  const event = await promise
-  // @ts-ignore
-  expect(event.data).toBe('test')
-  port1.close()
-  port2.close()
-})
-
-test('wrap - onMessage', async () => {
-  const { port1, port2 } = new MessageChannel()
-  const ipc = IpcChildWithMessagePort.wrap(port1)
-  const promise = new Promise((resolve) => {
-    ipc.onMessage(resolve)
-  })
-  port2.postMessage('test')
-  const event = await promise
-  // @ts-ignore
-  expect(event.data).toBe('test')
-  port1.close()
-  port2.close()
+  expect(await IpcChildWithModuleWorkerAndMessagePort.listen()).toBe(port)
 })
