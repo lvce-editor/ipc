557384a1d5c4e40632677b45bd4c8aa2d7e069f2
Le Vivilet <levivlet@pm.me>
2024-12-23 16:12:02 +0100

    feature: improve lint

--- src/parts/Promises/Promises.ts
+++ src/parts/Promises/Promises.ts
@@ -9,6 +9,7 @@ export const withResolvers = <T>(): WithResolvers<T> => {
     _resolve = resolve
   })
   return {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     resolve: _resolve!,
     promise,
   }
