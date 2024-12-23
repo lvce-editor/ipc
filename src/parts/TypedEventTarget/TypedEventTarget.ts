8d716d39fbf606659b2ce9caabd154c7829ed443
Le Vivilet <levivlet@pm.me>
2024-12-23 16:15:57 +0100

    feature: improve lint

--- src/parts/TypedEventTarget/TypedEventTarget.ts
+++ src/parts/TypedEventTarget/TypedEventTarget.ts
@@ -1,12 +1,8 @@
-export type TypedEventTarget<EventMap extends object> = { new (): IntermediateEventTarget<EventMap> }
-
 // internal helper type
 interface IntermediateEventTarget<EventMap> extends EventTarget {
-  addEventListener<K extends keyof EventMap>(
-    type: K,
-    callback: (event: EventMap[K] extends Event ? EventMap[K] : never) => EventMap[K] extends Event ? void : never,
-    options?: boolean | AddEventListenerOptions,
-  ): void
+  addEventListener<K extends keyof EventMap>(type: K, callback: (event: EventMap[K]) => void, options?: boolean | AddEventListenerOptions): void
 
   addEventListener(type: string, callback: EventListenerOrEventListenerObject | null, options?: EventListenerOptions | boolean): void
 }
+
+export type TypedEventTarget<EventMap extends object> = { new (): IntermediateEventTarget<EventMap> }
