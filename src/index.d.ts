bccf67e553388a5f53ffe45c29c76caea2137a3a
Le Vivilet <levivlet@pm.me>
2024-12-28 10:07:46 +0100

    feature: add more exact types for ipc

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,36 +1,56 @@
-interface Ipc extends EventTarget {
+interface Ipc<T = any> extends EventTarget {
   readonly send: (message: any) => void
   readonly sendAndTransfer: (message: any, transfer: any) => void
-  readonly dispose: () => void
-  readonly isDisposed: () => void
+  readonly dispose: () => void | Promise<void>
+  readonly isDisposed: () => boolean
 }
 
-interface IpcChild {
-  readonly listen: any
-  readonly wrap: (rawIpc: any) => Ipc
+interface IpcChild<WrapOptions = any, ListenOptions = any> {
+  readonly listen: (listenOptions: ListenOptions) => Promise<WrapOptions> | WrapOptions
+  readonly wrap: (rawIpc: WrapOptions) => Ipc<WrapOptions>
 }
 
-export const IpcChildWithElectronMessagePort: IpcChild
-export const IpcChildWithElectronUtilityProcess: IpcChild
-export const IpcChildWithNodeForkedProcess: IpcChild
-export const IpcChildWithWebSocket: IpcChild
-export const IpcChildWithNodeWorker: IpcChild
-export const IpcChildWithRendererProcess2: IpcChild
-export const IpcChildWithModuleWorkerAndMessagePort: IpcChild
-export const IpcChildWithModuleWorker: IpcChild
-export const IpcChildWithMessagePort: IpcChild
-export const IpcChildWithElectronWindow: IpcChild
-
-interface IpcParent {
-  readonly create: any
-  readonly wrap: (rawIpc: any) => Ipc
+interface IpcParent<T = any> {
+  readonly create: (options: any) => Promise<T>
+  readonly wrap: (rawIpc: T) => Ipc<T>
 }
 
-export const IpcParentWithElectronUtilityProcess: IpcParent
-export const IpcParentWithNodeForkedProcess: IpcParent
-export const IpcParentWithNodeWorker: IpcParent
-export const IpcParentWithElectronMessagePort: IpcParent
-export const IpcParentWithMessagePort: IpcParent
-export const IpcParentWithWebSocket: IpcParent
-export const IpcParentWithModuleWorker: IpcParent
-export const IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug: IpcParent
+interface IpcChildWithMessagePortListenOptions {
+  readonly port: MessagePort
+}
+
+// MessagePort specific implementations
+export const IpcChildWithMessagePort: IpcChild<MessagePort, IpcChildWithMessagePortListenOptions>
+export const IpcParentWithMessagePort: IpcParent<MessagePort>
+
+interface IpcChildWithElectronMessagePortListenOptions {
+  readonly messagePort: import('electron').MessagePortMain
+}
+
+interface IpcChildWithElectronUtilityProcessListenOptions {}
+
+interface IpcChildWithElectronWindowListenOptions {}
+
+// Electron specific implementations
+export const IpcChildWithElectronMessagePort: IpcChild<import('electron').MessagePortMain, IpcChildWithElectronMessagePortListenOptions>
+export const IpcChildWithElectronUtilityProcess: IpcChild<import('electron').MessagePortMain, IpcChildWithElectronUtilityProcessListenOptions>
+export const IpcChildWithElectronWindow: IpcChild<Window, IpcChildWithElectronWindowListenOptions>
+
+// Node specific implementations
+export const IpcChildWithNodeWorker: IpcChild<import('worker_threads').MessagePort>
+export const IpcChildWithNodeForkedProcess: IpcChild<import('child_process').ChildProcess>
+export const IpcChildWithWebSocket: IpcChild<WebSocket>
+
+// Web specific implementations
+export const IpcChildWithModuleWorker: IpcChild<Worker>
+export const IpcChildWithModuleWorkerAndMessagePort: IpcChild<MessagePort>
+export const IpcChildWithRendererProcess2: IpcChild<MessagePort>
+
+// Parent implementations
+export const IpcParentWithElectronUtilityProcess: IpcParent<import('electron').MessagePortMain>
+export const IpcParentWithNodeForkedProcess: IpcParent<import('child_process').ChildProcess>
+export const IpcParentWithNodeWorker: IpcParent<import('worker_threads').Worker>
+export const IpcParentWithElectronMessagePort: IpcParent<Electron.MessagePortMain>
+export const IpcParentWithWebSocket: IpcParent<WebSocket>
+export const IpcParentWithModuleWorker: IpcParent<Worker>
+export const IpcParentWithModuleWorkerAndWorkaroundForChromeDevtoolsBug: IpcParent<Worker>
